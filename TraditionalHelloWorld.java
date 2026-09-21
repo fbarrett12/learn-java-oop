eea21c28b99dbf4a0ac3ea093e9738d50313bd0f
FredrickBarrett <f.barrett12@gmail.com>
2026-09-20 20:18:54 -0400

    used tradional syntax for Hello World

---
+++ TraditionalHelloWorld.java
@@ -0,0 +1,8 @@
+public class TraditionalHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("What is your name? ");
+        String name = System.console().readLine();
+        System.out.println("Hello, " + name);
+    }
+}
